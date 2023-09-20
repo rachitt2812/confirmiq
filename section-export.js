92ee278f399b5aebc0dff445ed83b496899ef672
www-data <www-data@ip-172-31-53-170.ec2.internal>
2023-09-20 11:41:16 +0000

    New Commit

---
+++ section-export.js
@@ -0,0 +1,19 @@
+const { Given, When, Then }= require('@cucumber/cucumber')
+
+// Scenario: Simple User successfully login
+Given('Simple Go to login page', function () {
+// hi  rachit
+// Your test goes here ;
+});
+When('Simple Enter user email {string} and password {string}', function (str1, str2) {
+  //Simple Enter user email "abc@gmail.com" and password "abc2@413251234e"
+  // Your test goes here ;
+});
+
+Then('Simple Click on login button', function () {
+// Your test goes here ;
+});
+Then('Simple Login successfully {string}', function (str1) {
+//Simple Login successfully "ab2@gmail.comma"
+// Your test goes here ;
+});
