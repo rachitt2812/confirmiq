8f846f4dd47c138504e2da018fb6b0a3e1d9bbc6
www-data <www-data@ip-172-31-53-170.ec2.internal>
2023-09-25 07:44:03 +0000

    new commit by ap

---
+++ test1_stepdefinitions.js.js
@@ -0,0 +1,19 @@
+const { Given, When, Then }= require('@cucumber/cucumber')
+
+// Scenario: Simple User successfully login
+Given('Simple Go to login page', function () {
+// hi  rachit today 21 sept
+// Your test goes here ;
+});
+When('Simple Enter user email {string} and password {string}', function (str1, str2) {
+  //Simple Enter user email "user1@gmail" and password "pass@1234"
+  // Your test goes here ;
+});
+
+Then('Simple Click on login button', function () {
+// Your test goes here ;
+});
+Then('Simple Login successfullyaaaaaaa {string}', function (str1) {
+  //Simple Login successfullyaaaaaaa "userrrrrr@gmail.com"
+  // Your test goes here ;
+});
---
+++ testrepeat_stepdefinitions.js.js
@@ -0,0 +1,20 @@
+const { Given, When, Then }= require('@cucumber/cucumber')
+
+// Scenario: User successfully login
+Given('Go to login page', function () {
+  // Your test goes here ;
+});
+
+When('Enter user email {string} and password {string}', function (email, password) {
+  //Enter user email "<email>" and password "<password>"
+  // Your test goes here ;
+});
+
+Then('Click on login button', function () {
+  // Your test goes here ;
+});
+
+Then('Login successfully {string}', function (email) {
+  //Login successfully "<email>"
+  // Your test goes here ;
+});
