d062fc7f2e68b48692f4a1e7b2a48a5931b6e4e0
www-data <www-data@ip-172-31-53-170.ec2.internal>
2023-10-09 09:12:10 +0000

    t93 test

---
+++ t93_stepdefinitions.js.js
@@ -0,0 +1,30 @@
+const { Given, When, Then }= require('@cucumber/cucumber')
+
+// Scenario: Example Kevin successfully  login
+Given('Example Go to login page', function () {
+ 
+ // Your test goes here ;
+});
+
+Given('This is Background step', function () {
+ 
+ // Your test goes here ;
+});
+
+When('Example Enter user email {string} and password {string}', function (email, password) {
+ 
+ //Example Enter user email "<email>" and password "<password>"
+ 
+ // Your test goes here ;
+});
+
+Then('Example Click on login button', function () {
+ 
+ // Your test goes here ;
+  // some test
+});
+
+Then('Example Login successfully', function () {
+ 
+ // Your test goes here ;
+});
