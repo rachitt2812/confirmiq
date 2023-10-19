633f17af762e219362c643a0ff53d542c368d0ed
www-data <www-data@ip-172-31-53-170.ec2.internal>
2023-10-19 12:04:18 +0000

    n

--- section_StepDefinitions.js
+++ section_StepDefinitions.js
@@ -4,21 +4,15 @@ const { Given, When, Then }= require('@cucumber/cucumber')
 Given('Simple Go to login page', function () {
 // Your test goes here ;
 });
-
 When('Simple Enter user email {string} and password {string}', function (str1, str2) {
- 
- //Simple Enter user email "abc@gmail.com" and password "abc2@413251234e"
- 
- // Your test goes here ;
+  //Simple Enter user email "abc@gmail.com" and password "abc2@413251234e"
+  // Your test goes here ;
 });
 
 Then('Simple Click on login button', function () {
 // Your test goes here ;
 });
-
-Then('Simple Login successfully {string}', function (str1) {
- 
- //Simple Login successfully "ab2@gmail.comma"
- 
- // Your test goes here aman;
+Then('Simple Login succaaessfully {string}', function (str1) {
+  //Simple Login succaaessfully "ab2@gmail.co"
+  // Your test goes here ;
 });
