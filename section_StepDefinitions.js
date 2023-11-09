c04498f96c22ed51579ae83c8be1862101915803
www-data <www-data@ip-172-31-53-170.ec2.internal>
2023-11-09 05:24:59 +0000

    a

--- section_StepDefinitions.js
+++ section_StepDefinitions.js
@@ -1,18 +1,38 @@
-const { Given, When, Then }= require('@cucumber/cucumber')
+const { When, And, Then }= require('@cucumber/cucumber')
 
-// Scenario: Simple User successfully login
-Given('Simple Go to login page', function () {
+// Scenario: Deliver car
+When('Car is allocated is performed', function () {
 // Your test goes here ;
 });
-When('Simple Enter user email {string} and password {string}', function (str1, str2) {
-  //Simple Enter user email "abc@gmail.com" and password "abc2@413251234e"
+And('I have key', function () {
   // Your test goes here ;
 });
 
-Then('Simple Click on login button', function () {
+Then('Car is allocated completes', function () {
 // Your test goes here ;
 });
-Then('Simple Login succaaessfully {string}', function (str1) {
-  //Simple Login succaaessfully "ab2@gmail.co"
+When('Platform Transportation is performed', function () {
+// Your test goes here ;
+});
+Then('Platform Transportation completes new change', function () {
   // Your test goes here ;
+});
+
+
+// Scenario: Reject invalid requests
+When('Review Request is performed where request is <request2>', function () {
+// Your test goes here ;
+});
+Then('Review Request completes with outcome <outcome2>', function () {
+// Your test goes here ;
+});
+When('Request Rejected is performed hello', function () {
+// Your test goes here ;
+});
+And('Hello', function () {
+  // Your test goes here ;
+});
+
+Then('Request Rejected completes', function () {
+// Your test goes here ;
 });
