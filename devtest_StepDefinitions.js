de5c288fe88f1ae1b0b5fdde73a224c0a80a147e
www-data <www-data@ip-172-31-53-170.ec2.internal>
2024-02-21 13:02:33 +0000

    commit step file 1832

--- devtest_StepDefinitions.js
+++ devtest_StepDefinitions.js
@@ -8,9 +8,10 @@ const { afterAll } = require('mocha');
 var driver = '';
 var capabilities = '';
 
+
 // Scenario: DataTable User successfully login
 Given('DataTable Go to login page', function () {
-   // driver setup
+    // driver setup
     capabilities = Capabilities.chrome();
     capabilities.set('chromeOptions', { "w3c": false });
     driver = new Builder().withCapabilities(capabilities).build();
@@ -18,32 +19,24 @@ Given('DataTable Go to login page', function () {
 });
 When('DataTable Login with different different users', function (dataTable) {
 //DataTable Login with different different users
-    const data = dataTable.hashes(); // Get the data as an object with the header values as keys
-
-    //  Iterate over each row of the DataTable
-    for (const row of data) {
-        const email = row.email;
-        const password = row.password;
-
-        const usernameElement = await driver.findElement(By.name('username'));
-        usernameElement.sendKeys(email);
-
-        const passwordElement = await driver.findElement(By.name('password'));
-        passwordElement.sendKeys(password);
-
-        const loginBtn = await driver.findElement(By.name('submitBtn'));
-        loginBtn.click();
-
-
-        const anchorElement = await driver.wait(
-            until.elementLocated(By.id('logoutbtn')),
-            5000 // Maximum wait time in milliseconds
-        );
-        await anchorElement.click();
-
-    }
-
+const data = dataTable.hashes(); // Get the data as an object with the header values as keys
+//  Iterate over each row of the DataTable
+for (const row of data) {
+const email = row.email;
+const password = row.password;
+const usernameElement = await driver.findElement(By.name('username'));
+usernameElement.sendKeys(email);
+const passwordElement = await driver.findElement(By.name('password'));
+passwordElement.sendKeys(password);
+const loginBtn = await driver.findElement(By.name('submitBtn'));
+loginBtn.click();
+const anchorElement = await driver.wait(
+until.elementLocated(By.id('logoutbtn')),
+5000 // Maximum wait time in milliseconds
+);
+await anchorElement.click();
+}
 });
 Then('DataTable close the chrome', function () {
- driver.quit();
+driver.quit();
 });
