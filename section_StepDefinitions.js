88d29123b7368d56cbbad31f0447d2b6e33610bf
www-data <www-data@ip-172-31-53-170.ec2.internal>
2024-02-21 11:01:12 +0000

    push section step def version2

--- section_StepDefinitions.js
+++ section_StepDefinitions.js
@@ -1,10 +1,9 @@
 const { Given, When, Then }= require('@cucumber/cucumber')
 
 // Scenario: Simple User successfully login
-Given('Simple Go to login new page', function () {
-  // Your test goes here ;
+Given('Simple Go to login page', function () {
+// Your test goes here ;
 });
-
 When('Simple Enter user email {string} and password {string}', function (str1, str2) {
   //Simple Enter user email "aman@gmail" and password "aman@1234"
   // Your test goes here ;
