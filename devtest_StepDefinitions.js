c61be7b2717a8f82db04d8ae1b320b77e8c186dd
www-data <www-data@ip-172-31-53-170.ec2.internal>
2024-02-21 11:20:28 +0000

    data table step vv

---
+++ devtest_StepDefinitions.js
@@ -0,0 +1,14 @@
+const { Given, When, Then }= require('@cucumber/cucumber')
+
+// Scenario: DataTable User successfully login
+Given('DataTable Go to login page', function () {
+// Your test goes here ;
+});
+When('DataTable Login waaith different different users', function (dataTable) {
+  //DataTable Login waaith different different users
+  // Your test goes here ;
+});
+
+Then('DataTable close the chrome', function () {
+// Your test goes here ;
+});
