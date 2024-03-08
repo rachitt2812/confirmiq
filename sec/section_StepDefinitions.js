a4145025a1171db1ab29d9ad004855ba78aaaa37
www-data <www-data@ip-172-31-53-170.ec2.internal>
2024-03-08 07:34:29 +0000

    aa

--- sec/section_StepDefinitions.js
+++ sec/section_StepDefinitions.js
@@ -5,12 +5,14 @@ Given('Simple Go to login page', function () {
 // Your test goes here ;
 });
 When('Simple Enter user email {string} and password {string}', function (str1, str2) {
-  //Simple Enter user email "aman@gmail" and password "aman@1234"
-  // Your test goes here ;
+ 
+ //Simple Enter user email "aman@gmail" and password "aman@1234"
+ 
+ // Your test goes here ;
 });
 
 Then('Simple Click on login button', function () {
-// Your test goes here ;
+// Your test goes aa ;
 });
 Then('Simple Login succesasfully {string}', function (str1) {
 //Simple Login succesasfully "aman@gmail"
