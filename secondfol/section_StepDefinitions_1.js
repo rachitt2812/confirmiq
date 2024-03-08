ddadad13a30f79d0fefc82856c7789902bb64cce
www-data <www-data@ip-172-31-53-170.ec2.internal>
2024-03-08 11:37:57 +0000

    a

---
+++ secondfol/section_StepDefinitions_1.js
@@ -0,0 +1,20 @@
+const { Given, When, Then }= require('@cucumber/cucumber')
+
+// Scenario: Simple User successfully login
+Given('Simple Go to login page', function () {
+// Your test goes here ;
+});
+When('Simple Enter user email {string} and password {string}', function (str1, str2) {
+ 
+ //Simple Enter user email "aman@gmail" and password "aman@1234"
+ 
+ // Your test goes here ;
+});
+
+Then('Simple Click on login button', function () {
+// Your test goes aa ;
+});
+Then('Simple Login succesasfully {string}', function (str1) {
+//Simple Login succeasfully "aman@gmail"
+// Your test goes here ;
+});
