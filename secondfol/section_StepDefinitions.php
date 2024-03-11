ff4c118ce761aa30436261cb4b98f9c90caa8b80
www-data <www-data@ip-172-31-53-170.ec2.internal>
2024-03-11 04:31:53 +0000

    push php scrit

---
+++ secondfol/section_StepDefinitions.php
@@ -0,0 +1,17 @@
+<?php 
+
+use Behat\Behat\Context\Context;
+
+ class FeatureContext implements Context 
+ { 
+public function theMakerHasStartedAGameWithTheWord($str1) {
+// the Maker has started a game with the word "silky"
+// Your test goes here ;
+}
+public function theBreakerJoinsTheMakersGame() {
+// Your test goes here ;
+}
+public function theBreakerMustGuessAWordWith5Characters() {
+// Your test goes here ;
+}
+}
