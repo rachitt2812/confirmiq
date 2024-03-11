d7e385d86cd218914949b63d8551f53aa777732d
rachitt2812 <145096619+rachitt2812@users.noreply.github.com>
2024-03-11 11:27:34 +0530

    Update section_StepDefinitions_1.js
    remove acoawfa

--- secondfol/secondfol/section_StepDefinitions_1.js
+++ secondfol/secondfol/section_StepDefinitions_1.js
@@ -15,6 +15,6 @@ Then('Simple Click on login button', function () {
 // Your test goes aa ;
 });
 Then('Simple Login succesasfully {string}', function (str1) {
-//Simple Login sucaaceasfuzlly "aman@gmail new hey now its pull working"
+//Simple Login sucaaceasfuzlly "aman@gmail yes work so remove it"
 // Your test goes here ;
 });
