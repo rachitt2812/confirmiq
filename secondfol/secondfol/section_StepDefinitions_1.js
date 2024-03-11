7b2419fc3bc237164cc0b379f5bafa60b6f53e97
rachitt2812 <145096619+rachitt2812@users.noreply.github.com>
2024-03-11 11:31:50 +0530

    Update section_StepDefinitions_1.js
    ca

--- secondfol/secondfol/section_StepDefinitions_1.js
+++ secondfol/secondfol/section_StepDefinitions_1.js
@@ -15,6 +15,6 @@ Then('Simple Click on login button', function () {
 // Your test goes aa ;
 });
 Then('Simple Login succesasfully {string}', function (str1) {
-//Simple Login sucaaceasfuzlly "aman@gmaila"
+//Simple Login sucaaceasfuzlly "aman@gmail am"
 // Your test goes here ;
 });
