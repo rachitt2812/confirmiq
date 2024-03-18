718e4b2bc69b5e94aa89983da5177311d14742f2
www-data <www-data@ip-172-31-53-170.ec2.internal>
2024-03-18 09:06:37 +0000

    dir feature

---
+++ newsection/section_StepDefinitions.js
@@ -0,0 +1,13 @@
+const { Given, When, Then } = require("@cucumber/cucumber");
+
+// Scenario: Breaker joins a game
+Given("the ameon has started a game with the word {string}", function (str1) {
+  //the ameon has started a game with the word "silky"
+  // Your test goes here ;
+});
+When("the demon joins the Maker's game", function () {
+  // Your test goes here ;
+});
+Then("the areafa must guess a word with 5 charaacters", function () {
+  // Your test goaes here ;
+});
