d41f392acd117eb0c6de4cf34d42a2d1e5eaf5d1
www-data <www-data@ip-172-31-53-170.ec2.internal>
2024-03-18 09:09:47 +0000

    ca

---
+++ newsection/childfolder/section_StepDefinitions.php
@@ -0,0 +1,20 @@
+<?php
+
+use Behat\Behat\Context\Context;
+
+class FeatureContext implements Context
+{
+    public function theAmeonHasStartedAGameWithTheWord($str1)
+    {
+        // the ameon has started a game with the word "silky"
+        // Your test goes here ;
+    }
+    public function theDemonJoinsTheMakersGame()
+    {
+        // Your test goes here ;
+    }
+    public function theAreafaMustGuessAWordWith5Charaacters()
+    {
+        // Your test goes here ;
+    }
+}
