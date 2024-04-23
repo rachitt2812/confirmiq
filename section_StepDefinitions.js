35d78130c98e1961f083405d10855fa2e92adc7f
www-data <www-data@ip-172-31-53-170.ec2.internal>
2024-04-23 11:15:28 +0000

    a

--- section_StepDefinitions.js
+++
@@ -1,17 +0,0 @@
-const { Given, When, And, Then }= require('@cucumber/cucumber')
-
-// Scenario: Vsagaaaaaa the list of available phones
-Given('naae anasapp is loaded', function () {
-// Your test goes here ;
-});
-When('I AaaaaabAaaaoawse the phone catalog', function () {
-// Your test goes here ;
-});
-And('I aAaaaaaaAaaoaan the {string} page', function (str1) {
-  //I aAaaaaaaAaaoaan the "Phone Selection" page
-  // Your test goes here ;
-});
-
-Then('I AaaaaahAaoAauld see a list of available phones', function () {
-// Your test goes here ;
-});
